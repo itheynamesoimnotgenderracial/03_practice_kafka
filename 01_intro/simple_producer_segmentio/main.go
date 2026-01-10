c0016076bfd11d42565efc9e7c50956bc2bd3232
hoobaslacker <cyrilelagumbay@gmail.com>
2026-01-10 15:20:47 +0800

    added confluentinc and segmentio kafka to the practice project

---
+++ 01_intro/simple_producer_segmentio/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	topic := "user-profiles-segio"
+
+	writer := &kafka.Writer{
+		Addr:  kafka.TCP("localhost:9092"),
+		Topic: topic,
+		// Balancer: ,
+	}
+
+	defer func() {
+		err := writer.Close()
+		if err != nil {
+			log.Fatalf("failed to close write: %v", err)
+		}
+	}()
+
+	fmt.Println("Producer started. Send messages to topic: ", topic)
+	for i := range 10 {
+		message := fmt.Sprintf("Message #%d for user", i)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		err := writer.WriteMessages(ctx, kafka.Message{
+			Value: []byte(message),
+		})
+		if err != nil {
+			log.Fatalf("failed to write message %d: %v", i, err)
+		}
+
+		fmt.Printf("Sent message: %s\n", message)
+
+		// A small delay between messages
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	fmt.Println("Finished sending messages!")
+}
